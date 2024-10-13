40166cf8a59633cb8e7f4aece2cc4dccfac713c7
Christian Oliveira <christianoliveira1908@hotmail.com>
2024-10-12 21:44:07 -0300

    Created entity Product

---
+++ Northwind.Domain/Entities/Product.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Northwind.Domain.Entities
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(40)]
+        public string Name { get; set; }
+
+        //public Supplier Supplier { get; set; } TODO: Criar entidade Supplier
+
+        [Required]
+        public Category Category { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string QuantityPerUnit { get; set; }
+
+        [Required]
+        [DataType(DataType.Currency)]
+        public decimal UnitPrice{ get; set; }
+
+        [Required]
+        public int UnitsInStock { get; set; }
+
+        [Required]
+        public int UnitsOnOrder { get; set; }
+
+        [Required]
+        public int ReorderLevel { get; set; }
+                
+        public bool Discontinued { get; set; } = false;
+    }
+}
