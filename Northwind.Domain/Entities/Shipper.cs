c8e468de7b5f93b415911b932242a7b2d7ef44bb
Christian Oliveira <christianoliveira1908@hotmail.com>
2024-10-13 13:44:30 -0300

    Created Shipper entity

---
+++ Northwind.Domain/Entities/Shipper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Domain.Entities
+{
+    [Table("Shippers")]
+    public class Shipper
+    {
+        [Column("ShipperID")]
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(40)]
+        public string CompanyName { get; set; } = default!;
+
+        [MaxLength(24)]
+        public string Phone { get; set; } = default!;
+
+    }
+}
