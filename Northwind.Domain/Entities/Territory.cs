51e76f76edc0cc5039ffbf86a63109492ef0ef73
Christian Oliveira <christianoliveira1908@hotmail.com>
2024-10-13 13:44:38 -0300

    Created Territory entity

---
+++ Northwind.Domain/Entities/Territory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Domain.Entities
+{
+    [Table("Territories")]
+    public class Territory
+    {
+        [Required]
+        [Column("TerritoryId")]
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Description { get; set; } = default!;
+
+        [Required]
+        [ForeignKey(nameof(Region))]
+        public Region Region { get; set; } = default!;
+    }
+}
