f09fadb2b8a76727da473efec512ced906af7916
Christian Oliveira <christianoliveira1908@hotmail.com>
2024-10-12 21:33:36 -0300

    created category entity

---
+++ Northwind.Domain/Entities/Category.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Northwind.Domain.Entities
+{
+    public class Category
+    {
+        [Key]
+        [Column("CategoryID")]
+        public int Id { get; set; }
+
+        [MaxLength(15)]
+        [Column("CategoryName")]
+        [Required]
+        public string Name { get; set; }
+
+        [MaxLength(1073741823)]
+        [Required]
+        public string Description { get; set; }
+
+        [DataType(DataType.Upload)]
+        [Required]
+        public byte[] Picture { get;set; }
+    }
+}
