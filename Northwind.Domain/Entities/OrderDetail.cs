0f90a3ba82b1bebded47261d19eee20d26882597
Christian Oliveira <christianoliveira1908@hotmail.com>
2024-10-13 13:43:00 -0300

    Created OrderDetails.

---
+++ Northwind.Domain/Entities/OrderDetail.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Domain.Entities
+{
+    [Table("OrderDetails")]
+    public class OrderDetail
+    {
+        [Column("OrderID")]
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey(nameof(Product))]
+        [Required]
+        public Product Product { get; set; }
+
+        [DataType(DataType.Currency)]
+        public decimal UnitPrice { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+        [Required]
+        public float Discount { get; set; }
+
+    }
+}
