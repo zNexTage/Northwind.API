a5807cc9c08e1573481394e6a664abb487ab483c
Christian Oliveira <christianoliveira1908@hotmail.com>
2024-10-13 13:04:48 -0300

    Created Region entity

---
+++ Northwind.Domain/Entities/Region.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Domain.Entities
+{
+    public class Region
+    {
+        [Required]
+        [Column("RegionId")]
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [Column("RegionDescription")]
+        public int Description { get; set; }
+    }
+}
